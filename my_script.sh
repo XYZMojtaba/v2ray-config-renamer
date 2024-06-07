a9305ac56eb3c070fbb8de1c1f46b5df9eeee26e
XYZMojtaba <mojtabamirbaqeri961@gmail.com>
2024-06-07 12:49:37 +0330

    Add files via upload

---
+++ my_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the Downloads folder
+cd ~/storage/shared/Download
+
+# Download the v2ray-rename.sh script
+curl -O https://raw.githubusercontent.com/XYZMojtaba/v2ray-config-renamer/main/rename_v2ray_configs.sh
+
+# Set execute permission
+chmod +x rename_v2ray_configs.sh
+
+# Run the script
+./rename_v2ray_configs.sh
